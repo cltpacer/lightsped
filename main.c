92b624c5baf01be0afa88431629953e3729548ba
David Tang   唐仲龍 <30607803+cltpacer@users.noreply.github.com>
2018-06-24 08:09:29 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+int main()
+{
+double R = 6356751;  // in meter
+
+double PI = 3.14159;
+
+double speed_of_earth ;
+
+speed_of_earth = 2*PI*R/ ( 86400 );    // in meter/sec
+
+printf("-------------------\n"); 
+printf("sp of earth = %f meter/sec \n -------------\n", speed_of_earth ); // speed of sound  !!
+
+printf(" 100 years ago, the Exp for Eistan theory for light speed Can be wrong !! \n");
+
+
+
+
+
+
+
+}
+
+
